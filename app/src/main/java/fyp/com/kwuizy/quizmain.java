e8975aa485f1fe63b743ee21c7705409aee6e352
mattalexander125 <44009770+mattalexander125@users.noreply.github.com>
2019-04-16 16:48:47 +0100

    Updated Code
    Signed-off-by: mattalexander125 <44009770+mattalexander125@users.noreply.github.com>

---
+++ app/src/main/java/fyp/com/kwuizy/quizmain.java
@@ -0,0 +1,13 @@
+package fyp.com.kwuizy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quizmain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quizmain);
+    }
+}
