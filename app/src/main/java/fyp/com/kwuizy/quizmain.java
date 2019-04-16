ec53886bc5ad2699419b544d9fc33b502544ef91
Matthew Alexander <alexander-m4@email.ulster.ac.uk>
2019-04-16 18:26:54 +0100

    Updated Code
    Signed-off-by: mattalexander125 <44009770+mattalexander125@users.noreply.github.com>

--- app/src/main/java/fyp/com/kwuizy/quizmain.java
+++ app/src/main/java/fyp/com/kwuizy/quizmain.java
@@ -2,12 +2,56 @@ package fyp.com.kwuizy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class quizmain extends AppCompatActivity {
 
+    private TextView mScoreView;
+    private TextView mQuestion;
+
+    private Button mButtonChoice1, mButtonChoice2, mButtonChoice3, mButtonChoice4;
+
+    private int mScore;
+    private String mAnswer;
+
+    private Firebase mQuestionRef, mChoice1Ref, mChoice2Ref, mCHoice3Ref, mChoice4Ref , mAnswerRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quizmain);
+
+        mScoreView = (TextView)findViewById(R.id.score);
+        mQuestion = (TextView)findViewById(R.id.question);
+
+        mButtonChoice1 = (Button)findViewById(R.id.choice1);
+        mButtonChoice2 = (Button)findViewById(R.id.choice2);
+        mButtonChoice3 = (Button)findViewById(R.id.choice3);
+        mButtonChoice4 = (Button)findViewById(R.id.choice4);
+
+        updateQuestion();
     }
+public void updateQuestion(){
+        mQuestionRef = new Firebase("https://console.firebase.google.com/u/0/project/kwuizy-s125/database/kwuizy-s125/data");
+
+        mQuestionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String question = dataSnapshot.getValue(String.class);
+                mQuestion.setText(question);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+}
+
 }
